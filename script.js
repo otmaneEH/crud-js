dd929db04777df83338fd6c01b851cd09b3aea4d
otmaneelhaddaji <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-05-17 10:32:51 +0100

    function for validation from w3chools

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function verifier(){
-    var nom=document.getElementById("")
-
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
 }
