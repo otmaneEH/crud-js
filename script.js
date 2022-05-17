4fdcccd817b3bc12d0b39699e66fe7cdd645d1e5
otmaneelhaddaji <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-05-17 12:12:44 +0100

    2inputs

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function validateForm() {
-    let name =document.getElementById("firstName").value;
-    if(name==''){
+    let firstName =document.getElementById("firstName").value;
+    let lastNmae =document.getElementById("lastNmae").value;
+    // let ville =document.getElementById("ville").value;
+    // let type=document.querySelectorAll('input[name="type"]')
+if(firstName==''){
         alert("u have to entre ur name");
 
 
         return false
     }
+    if(lastNmae==''){
+        alert("u have to entre ur lastNmae");
+
+
+        return false
+    }
+
+
 }
