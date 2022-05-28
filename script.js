e7285acc43f345ada995deb07f0c8b4c4d6fc2cb
elhaddaji_Otmane <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-05-28 20:46:58 +0100

    mab

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 function validateForm() {
-    let firstName =document.getElementById("firstName").value;
-    let lastNmae =document.getElementById("lastNmae").value;
-    // let ville =document.getElementById("ville").value;
-    // let type=document.querySelectorAll('input[name="type"]')
-if(firstName==''){
+    let firstName = document.getElementById("firstName").value;
+    let lastNmae = document.getElementById("lastNmae").value;
+    let vill = document.getElementById("ville").value;
+    // let type = document.getElementsByTagName("type").value;
+
+
+    if (firstName == '') {
         alert("u have to entre ur name");
 
 
         return false
     }
-    if(lastNmae==''){
+    if (lastNmae == '') {
         alert("u have to entre ur lastNmae");
 
 
         return false
     }
 
-
 }
 function insert() //function for insert
 {
-    // document.getElementById("table").style.display="block";
-    
+
     var table = document.getElementById("table");
     var row = table.insertRow(-1);
     var firstname = row.insertCell(0);
     var lastname = row.insertCell(1);
-    // var amt = row.insertCell(2);
+    var ville = row.insertCell(2);
+    // var type = row.insertCell(2);
     firstname.innerHTML = document.getElementById("firstName").value;
     lastname.innerHTML = document.getElementById("lastNmae").value;
-    // amt.innerHTML = document.getElementById("amount").value;
-    
+    ville.innerHTML = document.getElementById("ville").value;
+    // type.innerHTML = document.getElementById("type").value;
+
     return false;
 }
